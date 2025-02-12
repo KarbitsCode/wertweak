826898099384ab23529b78b7a11e1c4fed2cd70f
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-02-12 14:49:32 +0800

    Add make.bat

---
+++ make.bat
@@ -0,0 +1,4 @@
+msbuild Source\WerTweak.sln /p:Configuration=Debug;Platform=x64
+msbuild Source\WerTweak.sln /p:Configuration=Debug;Platform=x86
+msbuild Source\WerTweak.sln /p:Configuration=Release;Platform=x64
+msbuild Source\WerTweak.sln /p:Configuration=Release;Platform=x86
