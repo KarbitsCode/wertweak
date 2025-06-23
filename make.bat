2e39d8ef216a08106f1a465b1427d39e4141d9f8
KarbitsCode <107671693+KarbitsCode@users.noreply.github.com>
2025-06-23 13:45:06 +0800

    Delete make.bat

--- make.bat
+++
@@ -1,4 +0,0 @@
-msbuild Source\WerTweak.sln /p:Configuration=Debug;Platform=x64
-msbuild Source\WerTweak.sln /p:Configuration=Debug;Platform=x86
-msbuild Source\WerTweak.sln /p:Configuration=Release;Platform=x64
-msbuild Source\WerTweak.sln /p:Configuration=Release;Platform=x86
